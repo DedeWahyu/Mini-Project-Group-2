0ef4c06ff35a6bf723b1f67ca594a247b495f8ca
trynxID <sidiq.firman48@gmail.com>
2024-03-24 22:57:50 +0700

    update offset

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
   // Menambahkan kelas active ke elemen fitur saat menggulir
   $(window).scroll(function () {
     var scrollPos = $(document).scrollTop();
-    var offset = 70; // Ubah nilai offset sesuai kebutuhan
+    var offset = 76; // Ubah nilai offset sesuai kebutuhan
     $(".fitur a").each(function () {
       var currLink = $(this);
       var refElement = $(currLink.attr("href"));
@@ -41,7 +41,7 @@ $(document).ready(function () {
       } else {
         $("html, body").animate(
           {
-            scrollTop: $(target).offset().top - 55,
+            scrollTop: $(target).offset().top - 70,
           },
           1000
         );
