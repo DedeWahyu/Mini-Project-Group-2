1e213d172e1e8f253e2f3618426483a6d1749577
trynxID <sidiq.firman48@gmail.com>
2024-03-25 22:20:48 +0700

    update to jquery

--- script.js
+++ script.js
@@ -79,12 +79,12 @@ $(document).ready(function () {
     $(window).scrollTop(0);
   });
 
-  // Seleksi elemen-elemen DOM yang diperlukan
-  const wrapper = document.querySelector(".wrapper"); // Wrapper carousel
-  const carousel = document.querySelector(".carousel"); // Kontainer carousel
-  const firstCardWidth = carousel.querySelector(".card").offsetWidth; // Lebar kartu pertama dalam carousel
-  const arrowBtns = document.querySelectorAll(".wrapper i"); // Tombol panah kiri dan kanan
-  const carouselChildrens = [...carousel.children]; // Anak-anak dari carousel dalam bentuk array
+  // Seleksi elemen-elemen jQuery yang diperlukan
+  const wrapper = $(".wrapper"); // Wrapper carousel
+  const carousel = $(".carousel"); // Kontainer carousel
+  const firstCardWidth = $(".carousel .card").outerWidth(); // Lebar kartu pertama dalam carousel
+  const arrowBtns = $(".wrapper i"); // Tombol panah kiri dan kanan
+  const carouselChildrens = carousel.children(); // Anak-anak dari carousel
 
   // Variabel untuk menandai status drag dan autoplay
   let isDragging = false,
@@ -94,116 +94,114 @@ $(document).ready(function () {
     timeoutId;
 
   // Mendapatkan jumlah kartu yang dapat ditampilkan dalam carousel sekaligus
-  let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+  let cardPerView = Math.round(carousel.outerWidth() / firstCardWidth);
 
   // Menyisipkan salinan kartu terakhir ke awal carousel untuk infinite scrolling
-  carouselChildrens
-    .slice(-cardPerView)
-    .reverse()
-    .forEach((card) => {
-      carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
-    });
+  carouselChildrens.slice(-cardPerView).clone().prependTo(carousel);
 
   // Menyisipkan salinan kartu pertama ke akhir carousel untuk infinite scrolling
-  carouselChildrens.slice(0, cardPerView).forEach((card) => {
-    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
-  });
+  carouselChildrens.slice(0, cardPerView).clone().appendTo(carousel);
 
   // Menggeser carousel ke posisi yang tepat untuk menyembunyikan beberapa kartu duplikat pertama pada Firefox
-  carousel.classList.add("no-transition");
-  carousel.scrollLeft = carousel.offsetWidth;
-  carousel.classList.remove("no-transition");
+  carousel.addClass("no-transition");
+  carousel.scrollLeft(carousel.outerWidth());
+  carousel.removeClass("no-transition");
 
   // Menambahkan event listener untuk tombol panah untuk menggeser carousel ke kiri dan kanan
-  arrowBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      carousel.scrollLeft +=
-        btn.id == "left" ? -firstCardWidth : firstCardWidth;
-    });
+  arrowBtns.on("click", function () {
+    carousel.scrollLeft(
+      carousel.scrollLeft() +
+        ($(this).attr("id") === "left" ? -firstCardWidth : firstCardWidth)
+    );
   });
 
   // Fungsi untuk memulai drag
   const dragStart = (e) => {
     isDragging = true;
-    carousel.classList.add("dragging");
+    carousel.addClass("dragging");
     // Merekam posisi kursor awal dan posisi scroll awal carousel
     startX = e.pageX;
-    startScrollLeft = carousel.scrollLeft;
+    startScrollLeft = carousel.scrollLeft();
   };
 
   // Fungsi untuk menangani peristiwa dragging
   const dragging = (e) => {
     if (!isDragging) return; // Jika isDragging adalah false, keluar dari fungsi
     // Memperbarui posisi scroll carousel berdasarkan pergerakan kursor
-    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+    carousel.scrollLeft(startScrollLeft - (e.pageX - startX));
   };
 
   // Fungsi untuk menghentikan drag
   const dragStop = () => {
     isDragging = false;
-    carousel.classList.remove("dragging");
+    carousel.removeClass("dragging");
   };
 
   // Fungsi untuk infinite scrolling
   const infiniteScroll = () => {
     // Jika carousel berada di awal, geser ke akhir
-    if (carousel.scrollLeft === 0) {
-      carousel.classList.add("no-transition");
-      carousel.scrollLeft = carousel.scrollWidth - 2 * carousel.offsetWidth;
-      carousel.classList.remove("no-transition");
+    if (carousel.scrollLeft() === 0) {
+      carousel
+        .addClass("no-transition")
+        .scrollLeft(carousel[0].scrollWidth - 2 * carousel.outerWidth())
+        .removeClass("no-transition");
     }
     // Jika carousel berada di akhir, geser ke awal
     else if (
-      Math.ceil(carousel.scrollLeft) ===
-      carousel.scrollWidth - carousel.offsetWidth
+      Math.ceil(carousel.scrollLeft()) ===
+      carousel[0].scrollWidth - carousel.outerWidth()
     ) {
-      carousel.classList.add("no-transition");
-      carousel.scrollLeft = carousel.offsetWidth;
-      carousel.classList.remove("no-transition");
+      carousel
+        .addClass("no-transition")
+        .scrollLeft(carousel.outerWidth())
+        .removeClass("no-transition");
     }
 
     // Hapus timeout yang ada & mulai autoplay jika mouse tidak berada di atas carousel
     clearTimeout(timeoutId);
-    if (!wrapper.matches(":hover")) autoPlay();
+    if (!wrapper.is(":hover")) autoPlay();
   };
 
   // Fungsi untuk autoplay
   const autoPlay = () => {
     if (window.innerWidth < 800 || !isAutoPlay) return; // Keluar jika lebar window lebih kecil dari 800 atau isAutoPlay adalah false
     // Autoplay carousel setiap 2500 ms
-    timeoutId = setTimeout(() => (carousel.scrollLeft += firstCardWidth), 2500);
+    timeoutId = setTimeout(
+      () => carousel.scrollLeft(carousel.scrollLeft() + firstCardWidth),
+      2500
+    );
   };
   autoPlay();
 
   // Menambahkan event listener untuk mousedown, mousemove, mouseup, scroll, mouseenter, dan mouseleave
-  carousel.addEventListener("mousedown", dragStart);
-  carousel.addEventListener("mousemove", dragging);
-  document.addEventListener("mouseup", dragStop);
-  carousel.addEventListener("scroll", infiniteScroll);
-  wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
-  wrapper.addEventListener("mouseleave", autoPlay);
+  carousel.on("mousedown", dragStart);
+  carousel.on("mousemove", dragging);
+  $(document).on("mouseup", dragStop);
+  carousel.on("scroll", infiniteScroll);
+  wrapper.on("mouseenter", () => clearTimeout(timeoutId));
+  wrapper.on("mouseleave", autoPlay);
 
-  $(".cek-harga-btn").click(function() {
+  // Menggunakan event delegation untuk menangani klik pada tombol "cek-harga-btn" yang dinamis
+  wrapper.on("click", ".cek-harga-btn", function () {
     var card = $(this).closest(".card");
     var namaProduk = card.find("h2").text();
     var deskripsiProduk = card.find(".deskripsi").text();
     var hargaProduk = card.find(".harga").text();
-  
+
     // Tampilkan SweetAlert dengan detail produk
     Swal.fire({
-      title: 'Detail Produk',
+      title: "Detail Produk",
       html: `
-        <div>
-          <b>Nama:</b> ${namaProduk}<br>
-          <b>Deskripsi:</b> ${deskripsiProduk}<br>
-          <b>Harga:</b> ${hargaProduk}
-        </div>
-      `,
+       <div>
+         <b>Nama:</b> ${namaProduk}<br>
+         <b>Deskripsi:</b> ${deskripsiProduk}<br>
+         <b>Harga:</b> ${hargaProduk}
+       </div>
+     `,
       icon: false,
-      confirmButtonText: 'Tutup'
+      confirmButtonText: "Tutup",
     });
   });
-  
 });
 function toggleMenu() {
   var menu = $("#navbar-menu");
